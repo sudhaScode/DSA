74a00ff9cfe57eedb0cf3f6061dd6a615f630c85
sudhaScode <sunriser7995@gmail.com>
2023-11-29 11:52:42 +0530

    soring algo

--- DSA_JS/Reverse.js
+++ DSA_JS/Reverse.js
@@ -1,7 +1,7 @@
 
 let  result=0;
 
-function reverse(n){
+/*function reverse(n){
     while(n!=0){
        var tmp = Math.floor(n%10);
        result = result*10 + tmp;
@@ -11,5 +11,17 @@ function reverse(n){
 
     return result;
 };
-result = reverse(792638);
-console.log("The reversed number "+ result);
+//result = reverse(792638);
+//console.log("The reversed number "+ result);*/
+
+function reverseNum(num){
+    num = num +'';
+
+ return num.split("").reverse().join("");	
+// join works like if no argument is passed to it it will concat the string with comma(,) or nothing given
+// between "" double qoutes it concats without any extra char
+// join works like if any argument is passed to it, "*" it concats with that char
+}
+console.log(reverseNum(3234243));
+
+
